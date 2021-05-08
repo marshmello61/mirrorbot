dbb9307f6722a92c8025c9af7daf7433f36cf96d
Arnav Puranik <puranikarnav@gmail.com>
2021-05-08 20:18:29 +0530

    Update authorize.py

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -19,18 +19,18 @@ def authorize(update,context):
             if chat_id not in AUTHORIZED_CHATS:
                 file.write(f'{chat_id}\n')
                 AUTHORIZED_CHATS.add(chat_id)
-                msg = 'Chat authorized'
+                msg = 'Congratulations! This Chat has been successfully authorized'
             else:
-                msg = 'Already authorized chat'
+                msg = 'This Chat is Already authorized!'
         else:
             # Trying to authorize someone in specific
             user_id = reply_message.from_user.id
             if user_id not in AUTHORIZED_CHATS:
                 file.write(f'{user_id}\n')
                 AUTHORIZED_CHATS.add(user_id)
-                msg = 'Person Authorized to use the bot!'
+                msg = 'This guy is now Authorized to use the bot!'
             else:
-                msg = 'Person already authorized'
+                msg = 'This guy is already authorized'
         sendMessage(msg, context.bot, update)
 
 
@@ -42,17 +42,17 @@ def unauthorize(update,context):
         chat_id = update.effective_chat.id
         if chat_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(chat_id)
-            msg = 'Chat unauthorized'
+            msg = 'Sadly, This Chat unauthorized'
         else:
-            msg = 'Already unauthorized chat'
+            msg = 'This is an unauthorized chat'
     else:
         # Trying to authorize someone in specific
         user_id = reply_message.from_user.id
         if user_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(user_id)
-            msg = 'Person unauthorized to use the bot!'
+            msg = 'This Guy is now unauthorized to use the bot!'
         else:
-            msg = 'Person already unauthorized!'
+            msg = 'This Guy is already unauthorized!'
     with open('authorized_chats.txt', 'a') as file:
         file.truncate(0)
         for i in AUTHORIZED_CHATS:
